06a03f45c27ba01728543763f4aeb38d6329e543
meorphis <eric@stainless.com>
2025-07-17 18:59:08 -0400

    Revert "feat(api): update via SDK Studio"
    This reverts commit 6cdcc6a36b9dde2117295ee7bcb9a3bc15571779.

--- src/gradientai/_utils/_resources_proxy.py
+++ src/gradientai/_utils/_resources_proxy.py
@@ -7,17 +7,17 @@ from ._proxy import LazyProxy
 
 
 class ResourcesProxy(LazyProxy[Any]):
-    """A proxy for the `do_gradientai.resources` module.
+    """A proxy for the `gradientai.resources` module.
 
-    This is used so that we can lazily import `do_gradientai.resources` only when
-    needed *and* so that users can just import `do_gradientai` and reference `do_gradientai.resources`
+    This is used so that we can lazily import `gradientai.resources` only when
+    needed *and* so that users can just import `gradientai` and reference `gradientai.resources`
     """
 
     @override
     def __load__(self) -> Any:
         import importlib
 
-        mod = importlib.import_module("do_gradientai.resources")
+        mod = importlib.import_module("gradientai.resources")
         return mod
 
 
